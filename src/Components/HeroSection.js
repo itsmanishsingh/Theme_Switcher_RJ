d394286f3dc88b58a3ef5c33645818eefcb88a3f
itsmanishsingh <itsmanishsingh6@gmail.com>
2024-01-14 00:41:22 +0530

    Learning Initiate

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,9 @@
+import React , {useContext} from "react"
+import ThemeContext from "../context/ThemeContext"
+import AppTheme from "../Colors"
+
+const HeroSection = ()=>{
+
+}
+
+export default HeroSection;
---
+++ src/Components/ThemeToggler.js
@@ -0,0 +1,16 @@
+import react , {useContext} from "react"
+import ThemeContext from "../context/ThemeContext"
+
+const ThemeToggler = ()=>{
+    const [themeMode , setThemeMode] = useContext(ThemeContext)
+
+    return(
+        <div onClick = {()=>{
+            setThemeMode(themeMode === "lisght" ? "dark" : "light")
+        }}>
+            <span>{themeMode === "light" ? "Turn Off" : "Lights On"}</span>
+        </div>
+    )
+}
+
+export default ThemeToggler;
